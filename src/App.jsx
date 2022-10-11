04f1355ad7a5acdb131593eb7508c6bd4bd1c545
FatimaVO <fatima.vazquez.fv.fv@gmail.com>
2022-10-11 16:16:36 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <main>
+        Tiktok clone
+      </main>
+    </div>
+  )
+}
+
+export default App
